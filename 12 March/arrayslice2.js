1b871e20809188b3ca267a83985585a82ac9488c
Anuragchaturvedi863 <anuragc227@gmail.com>
2025-03-12 21:25:11 +0530

    update

--- 12 March/arrayslice2.js
+++ 12 March/arrayslice2.js
@@ -1,10 +1,9 @@
 let ar1=[1,2,3,45,67,5,89,10,34,76,87]
 let part=ar1.slice(1,10)
-console.log(part)
 for(let i=0;i<11;i++)
 {
-    if(part%2==0)
+    if(part[i]%2==0)
     {
-        console.log(part)
+        console.log(part[i])
     }
 }

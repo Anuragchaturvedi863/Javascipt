8d58232d5af22a804b64958dabd548cf36e4cb18
Anuragchaturvedi863 <anuragc227@gmail.com>
2025-03-20 16:31:01 +0530

    update

---
+++ 20 March/object.js
@@ -0,0 +1,12 @@
+let emp ={
+    emp_name  :"Manish"
+    emp_age : 90
+    emp_contact : 2321
+    emp_designation : "Jr developer" 
+    emp_account {
+        emp_balance : 2000
+        emp_address : "pari bazaar"
+    }
+}
+
+console.log(emp.emp_account:emp_address)

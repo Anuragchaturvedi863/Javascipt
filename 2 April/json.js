a3dbdd4c7f8dae5744979fe540b15b164b530c53
Anuragchaturvedi863 <anuragc227@gmail.com>
2025-04-09 16:33:33 +0530

    update

--- 2 April/json.js
+++ 2 April/json.js
@@ -10,8 +10,9 @@ let fetchData= async()=>{
         <td> ${e.name} </td>
         <td> ${e.age} </td>
         <td onclick="Del('${e.id}')"> Delete </td>
+        <td onclick="FormFill('${e.id}')"> Update </td>
         </tr>
-        `
+        ` 
     })
 }
 
@@ -19,4 +20,62 @@ let Del=(id)=>{
     let url=`http://localhost:3000/hotel/${id}`
     fetch(url,{method:"DELETE"})
 }
-fetchData()
+
+let book=()=>{
+    let inpname=document.querySelector("#name").value
+    let inpage=document.querySelector("#age").value
+    let url="http://localhost:3000/hotel"
+    fetch(url,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(
+            {
+                "name":inpname,
+                "age":inpage
+            }
+        )
+    })
+
+    location.href="json.html"
+    return false
+}
+
+
+
+let FormFill= async(id)=>{
+    let url = `http://localhost:3000/hotel/${id}`
+    let res = await fetch(url, {method:"GET"})
+    let data = await res.json()
+    console.log(data)
+    let FormData = document.querySelector("#datashow")
+    FormData.innerHTML=`
+    Enter Name: <input type="text" id="upname" value="${data.name}"> <br> <br>    
+    Enter Age: <input type="text" id="upage" value="${data.age}"> <br> <br> 
+    <input type="submit" value="Update Now" onclick="return FinalUpdate('${data.id}')">   
+    `
+
+     
+}
+
+let FinalUpdate=(id)=>{
+    let inpname=document.querySelector("#upname").value
+    let inpage=document.querySelector("#upage").value
+    let url=`http://localhost:3000/hotel/${id}`
+    fetch(url,{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(
+            {
+                "name":inpname,
+                "age":inpage
+            }
+        )
+    })
+
+    location.href="json.html"
+    return false
+}
